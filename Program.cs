7b03bae3a33c1ac83dddfafdeb2dff32998df448
InGWet <kozakvlad97@gmail.com>
2022-06-14 14:50:32 +0300

    seminar

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿
+// Console.WriteLine("input integer number : ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = num + num;
+// Console.WriteLine("Result is " + result);
+
+// Console.WriteLine("input first number : ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("input second number : ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// int square = num2 * num2;
+
+// if(num1 == square){
+//     Console.WriteLine("first number");
+// }
+// else{
+//     Console.WriteLine("first number not");
+// }
+
+// Console.WriteLine("input first number or day: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// if(day < 1 || day > 7){
+//     Console.WriteLine("incorrect");
+// }
+// else{
+//     if (day == 1){
+//         Console.WriteLine("monday");
+//     }
+//     if (day == 2){
+//         Console.WriteLine("tuersday");
+//     }
+//     if (day == 3){
+//         Console.WriteLine("wednesday");
+//     }
+//     if (day == 4){
+//         Console.WriteLine("thursday");
+//     }
+//     if (day == 5){
+//         Console.WriteLine("friday");
+//     }
+//     if (day == 6){
+//         Console.WriteLine("saturday");
+//     }
+//     if (day == 7){
+//         Console.WriteLine("sunday");
+//     }
+
+// }
+
+// Console.WriteLine("input N");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int current;
+// if(num < 0){
+//     current=num;
+//     num = num *(-1);
+// }
+// else{
+//     current = -1 * num;
+// }
+
+// while(current < num-1){
+//     current++;
+//     Console.Write(current + " ");
+    
+// }
+
+// Console.WriteLine("input N");
+// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(num % 10);
