1f3ab7a9c75b831a1eab8b425bd1c148c68d9d7c
InGWet <kozakvlad97@gmail.com>
2022-06-14 15:21:40 +0300

    first home-work

---
+++ home-work/Program.cs
@@ -0,0 +1,16 @@
+﻿// 1 задание
+Console.WriteLine("Введите первое число");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1>num2){
+    Console.WriteLine("Наибольшее число {0} ", num1);
+    Console.WriteLine("Наименьщее число {0} ", num2);
+}
+else{
+    Console.WriteLine("Наибольшее число {0} ", num2);
+    Console.WriteLine("Наименьщее число {0} ", num1);
+}
+
