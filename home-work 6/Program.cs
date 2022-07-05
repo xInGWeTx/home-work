eb2f57df34e9ced52eda0ec2ce21697c4b818f3d
InGWet <kozakvlad97@gmail.com>
2022-07-05 06:58:39 +0300

    home-work 6

---
+++ home-work 6/Program.cs
@@ -0,0 +1,64 @@
+﻿// 1 задание Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+
+// Console.Write($"Введи число М(количество чисел): ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// int[] massiveNumbers = new int[m];
+
+// void InputNumbers(int m){
+// for (int i = 0; i < m; i++)
+//   {
+//     Console.Write($"Введи {i+1} число: ");
+//     massiveNumbers[i] = Convert.ToInt32(Console.ReadLine());
+//   }
+// }
+
+
+// int Comparison(int[] massiveNumbers)
+// {
+//   int count = 0;
+//   for (int i = 0; i < massiveNumbers.Length; i++)
+//   {
+//     if(massiveNumbers[i] > 0 ) count += 1; 
+//   }
+//   return count;
+// }
+
+// InputNumbers(m);
+
+// Console.WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
+
+// 2 задание. Написать программу преобразования десятичного числа в двоичное
+
+
+Console.Write("Введите число:   ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] ConvertToBinary(int num)
+{
+    int num2 = num;
+    int size = 0;
+    while (num > 0)
+    {
+        num = num / 2;
+        size++;
+    }
+
+    int[] binary = new int[size];
+    for (int i = size - 1; i >= 0; i--)
+    {
+        binary[i] = num2 % 2;
+        num2 = num2 / 2;
+    }
+    return binary;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write(arr[i]);
+    }
+}
+
+PrintArray(ConvertToBinary(number));
